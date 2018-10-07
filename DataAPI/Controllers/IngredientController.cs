d96791483cc48dd83b24b858dd76e9bfa9ce7c9e
Christopher Mooney <n9925279@qut.edu.au>
2018-10-07 15:50:37 +1000

    Added get user

---
+++ DataAPI/Controllers/IngredientController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAPI.Models;
+
+namespace DataAPI.Controllers
+{
+    public class IngredientController : ApiController
+    {
+        public IEnumerable<Ingredient> Get()
+        {
+            using (rpgfitnessDBContext dBContext = new rpgfitnessDBContext())
+            {
+                return dBContext.Ingredients.ToList();
+            }
+        }
+
+        public Ingredient Get(int id)
+        {
+            using (rpgfitnessDBContext dbContext = new rpgfitnessDBContext())
+            {
+                return dbContext.Ingredients.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ DataAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAPI.Models;
+
+namespace DataAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IEnumerable<Player> Get()
+        {
+            using (UserDBContext dBContext = new UserDBContext())
+            {
+                return dBContext.Players.ToList();
+            }
+        }
+
+        public Player Get(int id)
+        {
+            using (UserDBContext dBContext = new UserDBContext())
+            {
+                return dBContext.Players.FirstOrDefault(p => p.UserID == id);
+            }
+        }
+    }
+}
